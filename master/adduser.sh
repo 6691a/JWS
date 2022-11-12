f28bfab351ca27c785e5a6f7837bd4e9f8889dc1
root <root@master.8.8.8.8>
2022-11-12 12:26:10 +0000

    ansible, terraform, adduser and user pemkey setup shell script

---
+++ master/adduser.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+HOME_DIR=/home/$1
+
+sudo groupadd $1
+sudo useradd -m $1 -g $1
+
+cd ${HOME_DIR}
+
+mkdir .ssh
+chmod 700 .ssh
+
+if [-n "$2"];
+then
+ echo $2 > .ssh/authorized_keys
+else
+ssh-keygen -f ${HOME_DIR}/.ssh/id_rsa -t rsa -m pem -N ""
+ mv ${HOME_DIR}/.ssh/id_rsa ${HOME_DIR}/.ssh/$1.pem
+ mv ${HOME_DIR}/.ssh/id_rsa.pub ${HOME_DIR}/ssh/authorized_keys
+fi
