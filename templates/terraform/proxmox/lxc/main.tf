1007e8fc1b4d643de64b2a906835f0879663d47b
6691a <6691a@naver.com>
2022-11-13 22:22:16 +0900

    terraform proxmox lcx count

--- templates/terraform/proxmox/lxc/main.tf
+++ templates/terraform/proxmox/lxc/main.tf
@@ -1,10 +1,10 @@
 resource "proxmox_lxc" "lxcs" {
   count = local.hw.count
 
-  hostname = local.hw.name
-  cores = local.hw.cpu
-  memory = local.hw.memory
-  swap = local.hw.swap
+  hostname = local.hw.name[count.index]
+  cores = local.hw.cpu[count.index]
+  memory = local.hw.memory[count.index]
+  swap = local.hw.swap[count.index] ? local.hw.swap[count.index] : 512
 
   target_node = "proxmox"
   ostemplate = "local:vztmpl/${local.template.name}-${local.template.version}-standard_${local.template.version}-1_amd64.tar.gz"
@@ -14,19 +14,19 @@ resource "proxmox_lxc" "lxcs" {
     %{ endfor }
   EOF
 
-  unprivileged = local.hw.unprivileged ? true : false
+  unprivileged = local.hw.unprivileged[count.index] ? true : false
 
-  onboot = local.config.onboot
-  start = local.config.start
+  onboot = local.config.onboot[count.index] ? true : false
+  start = local.config.start[count.index] ? true : false
 
   rootfs {
-    storage = local.hw.disk.storage
-    size    = local.hw.disk.size
+    storage = local.hw.disk[count.index].storage
+    size    = local.hw.disk[count.index].size
   }
 
   network {
-    name   = local.hw.network.name
-    bridge = local.hw.network.bridge
+    name   = local.hw.network.name[count.index]
+    bridge = local.hw.network.bridge[count.index]
     ip     = local.hw.network.ip[count.index]
   }
 }
