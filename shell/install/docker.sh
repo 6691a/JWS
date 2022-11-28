5f5dcc9cbf06a627a87fc48ccafe9112fdc45720
6691a <6691a@naver.com>
2022-11-28 16:49:47 +0900

    master docker install script

---
+++ shell/install/docker.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Install dependencies
+sudo apt-get update && sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+# Add Docker’s official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Set up the stable repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker CE
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Use Docker without root
+sudo usermod -aG docker $USER
+
+
+
+# Docker restart
+sudo systemctl restart docker
