7ab461ec13778a50fd35ef62c96fed8ca31818f7
6691a <6691a@naver.com>
2022-11-28 16:57:05 +0900

    master docker install script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,5 +1,10 @@
+
 cat <<EOF >> ~/.bashrc
 export TF_VAR_FILE="$HOME/jws/secret/credentials.tfvars"
-
+export JWS_SCRIPT_DIR="$HOME/jws/shell"
+export JWS_ROOT_DIR="$HOEM/jws"
 EOF
+
+# Install Docker
+/bin/bash $JWS_SCRIPT_DIR/install/docker.sh
 
--- setup/web/terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "config_file" {
-  type = string
-  default = "./config.yaml"
-}
