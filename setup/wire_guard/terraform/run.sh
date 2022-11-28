2c16fc613b7b550f8a2e5842c15d2fbd7aa5858b
6691a <6691a@naver.com>
2022-11-28 01:01:16 +0000

    wire_guard bash run.sh test

--- setup/wire_guard/terraform/run.sh
+++ setup/wire_guard/terraform/run.sh
@@ -2,7 +2,9 @@
 
 readarray TF_VARS < $TF_VAR_FILE
 for (( i = 0; i < ${#TF_VARS[@]}; i++ )); do
-    TF_VARS[$i]='"-var=${TF_VARS[i]}"'
+    echo ${TF_VARS[i]}
+    TF_VARS[$i]=-var=${TF_VARS[i]}
 done
 
-echo "terraform apply ${TF_VARS[@]}"
+echo terraform apply ${TF_VARS[@]}
+terraform apply ${TF_VARS[@]}
