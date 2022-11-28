d38697a5729c2299f61fe21cedb167de3406e670
6691a <6691a@naver.com>
2022-11-28 07:18:56 +0000

    jws setup web server

---
+++ setup/web/terraform/main.tf
@@ -0,0 +1,36 @@
+resource "proxmox_lxc" "wire_guard" {
+
+  hostname = local.hw.name
+  cores = local.hw.cpu
+  memory = local.hw.memory
+  swap = try(local.hw.swap, 512)
+
+  target_node = "proxmox"
+  ostemplate = "local:vztmpl/${local.template.name}-${local.template.version}-standard_${local.template.version}-1_amd64.tar.gz"
+  password = var.proxmox_root_password
+  ssh_public_keys = <<EOF
+    %{ for s in local.ssh}
+      ${ s }
+    %{ endfor }
+  EOF
+
+  unprivileged = local.hw.unprivileged ? true : false
+
+  onboot = try(local.system.onboot, false)
+  start = try(local.system.start, false)
+
+  rootfs {
+    storage = local.disk.storage
+    size    = local.disk.size
+  }
+
+  network {
+    name   = try(local.network.name, "eth0")
+    bridge = try(local.network.bridge, "vmbr0")
+    ip     = try(local.network.ip, "dhcp")
+  }
+}
+
+# resource "local_file" "ansible_inventory" {
+#   content = templatefile("./templates/inventory.temp")
+# }
---
+++ setup/web/terraform/terraform.tf
@@ -0,0 +1,59 @@
+###################################################
+# Variables
+###################################################
+locals {
+  hw =  yamldecode(file(var.config_file)).hw
+  disk = yamldecode(file(var.config_file)).disk
+  network =  yamldecode(file(var.config_file)).network
+  system = yamldecode(file(var.config_file)).system
+  template = yamldecode(file(var.config_file)).template
+  ssh = yamldecode(file(var.config_file)).ssh
+}
+
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "proxmox_root_password" {
+  type = string
+  default = ""
+  sensitive = true
+}
+
+###################################################
+# Terrafrom
+###################################################
+
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.9.11"
+    }
+  }
+}
+
+###################################################
+# Providers
+###################################################
+# module "proxmox_setting" {
+#   source = "$TF_VAR_FILE"
+#   version = "1.0.0"
+# }
+
+provider "proxmox" {
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+}
+
