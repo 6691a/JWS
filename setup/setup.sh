d8e5a7c5560ccfd5f3b56ac4c2d7d1018fdf5444
6691a <6691a@naver.com>
2022-11-29 02:33:57 +0000

    setup.sh add docker install

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
 
-cat <<EOF >> ~/.bashrc
-export TF_VAR_FILE="$HOME/jws/secret/credentials.tfvars"
-export JWS_SCRIPT_DIR="$HOME/jws/shell"
-export JWS_ROOT_DIR="$HOEM/jws"
+HOME_DIR=$(echo $HOME)
+
+cat <<EOF >> $HOME_DIR/.bash_profile
+export JWS_ROOT_DIR=$HOME_DIR/jws
+export TF_VAR_FILE=$JWS_ROOT_DIR/secret/credentials.tfvars
+export JWS_SCRIPT_DIR=$JWS_ROOT_DIR/shell
 EOF
 
+
+source $HOME_DIR/.bash_profile
+
 # Install Docker
-/bin/bash $JWS_SCRIPT_DIR/install/docker.sh
+bash $JWS_SCRIPT_DIR/install/docker.sh
+
+
 
